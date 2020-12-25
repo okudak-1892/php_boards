1d82b6b2a0fa1fcaaef672e52eee0a0dc8f2acb1
okuda <aru@okudanoMacBook-Pro.local>
2020-12-25 18:19:51 +0900

    php memo app

---
+++ work/web/result.php
@@ -0,0 +1,14 @@
+<?php
+
+require('../app/functions.php');
+
+
+include('../app/_parts/_header.php');
+
+?>
+<p>メッセージが追加されました。</p>
+<p><a href="index.php">戻る</a></p>
+  
+<?php
+
+include('../app/_parts/_footer.php');
